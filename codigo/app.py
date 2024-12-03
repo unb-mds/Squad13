d158052c14f45a6a81c4627c397f70767558a7e6
Artur Mendonça Arruda <arturmendoncaarruda@gmail.com>
2024-12-03 11:36:22 -0300

    feat: Organização da tables com json e js

--- codigo/app.py
+++ codigo/app.py
@@ -36,29 +36,6 @@ def charts():
 # Página de tabelas
 @app.route('/tables')
 def tables():
-    # Exemplo de dados para a tabela
-    data = [
-        {'name': 'Alice', 'expense': 230},
-        {'name': 'Bob', 'expense': 150},
-        {'name': 'Charlie', 'expense': 320},
-        {'name': 'Daniel', 'expense': 500},
-        {'name': 'Eva', 'expense': 450},
-        {'name': 'Frank', 'expense': 300},
-        {'name': 'Grace', 'expense': 350},
-    ]
-    
-    search_query = request.args.get('search')
-    if search_query:
-        data = [item for item in data if search_query.lower() in item['name'].lower()]
-
-    # Obtemos os parâmetros de ordenação da URL
-    sort_by = request.args.get('sort', 'name')  # Se não houver, ordena por 'name' por padrão
-    sort_order = request.args.get('sort_order', 'asc')  # A ordem padrão é 'asc' (crescente)
-
-    # Ordenar a lista de acordo com o parâmetro de ordenação e a direção
-    reverse = True if sort_order == 'desc' else False
-    data.sort(key=lambda x: x[sort_by], reverse=reverse)
-
-    return render_template('tables.html', data=data, search_query=search_query, sort_by=sort_by, sort_order=sort_order)
+    return render_template('tables.html')
 if __name__ == '__main__':
     app.run(debug=True)
